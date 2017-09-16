0640d49940f344e45364f828fa38fb1f606f6476
Gabriel Marini <gabriel.g.marini@hotmail.com>
2017-09-16 14:33:07 -0300

    Finalização de 1º servidor node

--- startServerWeb.js
+++ startServerWeb.js
@@ -2,8 +2,6 @@ var http = require('http');
 var server = http.createServer(function(req,res){
     if(req.url == '/teste'){
         res.end("Teste 1");
-    }else if(req.url == "/loja"){
-        res.end("Loja");
     }else{
         res.end("Home");
     }
