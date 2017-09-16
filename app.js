d379f8fb7842db449a7bc91f8e5ec3952cf33665
Gabriel Marini <gabriel.g.marini@hotmail.com>
2017-09-16 15:09:29 -0300

    Add Modulo EJS e criação de templates dinâmicos para aplicação

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-
 app.listen(3000,function(){
     console.log("Server Online!");
+});
+app.set('view engine','ejs');
+//Rotas Manuais
+app.get('/loja',function(req,res){
+    res.render("loja/index");
 });
