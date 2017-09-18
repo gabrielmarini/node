802b96d41b89144bcae6e2f2b700fd17be031677
Gabriel Marini <gabriel.g.marini@hotmail.com>
2017-09-17 21:07:20 -0300

    Trabalhando com resposta do banco de dados

--- app/appRoutes.js
+++ app/appRoutes.js
@@ -1,5 +1,16 @@
 module.exports = function(app){
     app.get('/loja',function(req,res){
-        res.render("loja/index");
+        var mysql = require('mysql');
+        var conexao = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'node'
+        });
+
+        conexao.query('select * from livros',function(err,result){
+            res.render('loja/index',{lista:result});
+        });
+        conexao.end();
     });
 }
